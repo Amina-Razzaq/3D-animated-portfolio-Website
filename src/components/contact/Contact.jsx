ff63edcb4b45dcb2242744d02926c8e1be972798
Amina-Razzaq <aminarazzaq72@gmail.com>
2025-06-17 22:39:13 +0500

    emailjs addition

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -54,44 +54,61 @@ const Contact = () => {
   const isInView = useInView(ref, { margin: "-200px" });
 
   return (
-    <div className="contact" ref={ref} onSubmit={sendEmail}>
+    <div className="contact" ref={ref}>
       <div className="cSection">
         <motion.form
           ref={form}
+          onSubmit={sendEmail}
           variants={listVariant}
           animate={isInView ? "animate" : "initial"}
         >
           <motion.h1 variants={listVariant} className="cTitle">
             Let's keep in touch
           </motion.h1>
+
           <motion.div variants={listVariant} className="formItem">
             <label>Name</label>
-            <input type="text" name="user_username" placeholder="John Doe" />
+            <input
+              type="text"
+              name="user_username"
+              placeholder="John Doe"
+              required
+            />
+            <input type="hidden" name="name" value="John Doe" />
           </motion.div>
+
           <motion.div variants={listVariant} className="formItem">
             <label>Email</label>
             <input
               type="email"
               name="user_email"
               placeholder="john@gmail.com"
+              required
             />
+            <input type="hidden" name="email" value="john@gmail.com" />
           </motion.div>
+
           <motion.div variants={listVariant} className="formItem">
             <label>Message</label>
             <textarea
               rows={10}
               name="user_message"
               placeholder="Write your message..."
+              required
             ></textarea>
           </motion.div>
+
           <motion.button variants={listVariant} className="formButton">
             Send
           </motion.button>
+
           {success && <span>Your message has been sent!</span>}
           {error && <span>Something went wrong!</span>}
         </motion.form>
       </div>
-      <div className="cSection"><ContactSvg/></div>
+      <div className="cSection">
+        <ContactSvg />
+      </div>
     </div>
   );
 };
